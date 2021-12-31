272715e0f626fd0ac33cb7db06916182dad7ddcb
Braden Peterson <bnp82380@uga.edu>
2021-12-31 13:40:36 -0500

    Added method signatures for inherited methods/constructor from Game.java into ChessGame.java.

--- src/cs1302/game/ChessGame.java
+++ src/cs1302/game/ChessGame.java
@@ -9,13 +9,37 @@ import javafx.event.Event;
  * chess game board, multiple unique chess pieces, and player moves
  * available to two opponents in a typical game of chess.
  */
-public class ChessGame {
+public class ChessGame extends Game {
+
+    private static final int WIDTH = 800; //Width of game bounds
+    private static final int HEIGHT = 600; //Height of game bounds
 
     /**
-     * Constructs an instance of a {@code ChessGame}.
+     * Constructs a {@code ChessGame} object.
      */
     public ChessGame() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        super(WIDTH, HEIGHT, 60);
+        setLogLevel(Level.INFO);
     } //ChessGame
+
+    /**
+     * Initializes the scene graph for the {@code ChessGame} object.
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    protected void init() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    } //init
+
+    /**
+     * Updates the scene graph/game board with player and ball positions.
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    protected void update() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    } //update
 
 } //ChessGame
